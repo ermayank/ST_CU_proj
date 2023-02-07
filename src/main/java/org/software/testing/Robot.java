d7d1b77dfc762d3c185032dcdcf24a2091d37260
Krishna-Bhatt <krishna.bhatt@ssense.com>
2023-02-06 22:58:03 -0500

    Fixing breakage code

--- src/main/java/org/software/testing/Robot.java
+++ src/main/java/org/software/testing/Robot.java
@@ -5,19 +5,32 @@ import java.util.*;
 
 public class Robot {
 
-    int floorSize = 10;
-    int[] robotPosition = {0,0};
-    String direction = "north";
-    String pen = "up";
+    int floorSize;
+    int[] robotPosition;
+    String direction;
+    String pen;
     int[][] floor;
+    int initialized = 0;
+    public Robot(){
+
+    }
     //Method to Initialize
-    public void initializeFloor(int size){
-        floorSize = size;
-        floor = new int[floorSize][floorSize];
-        direction = "north";
-        pen = "up";
-        robotPosition[0] = 0;
-        robotPosition[1]=0;
+    public boolean initializeFloor(int size){
+        if(size>0)
+        {
+            floorSize = size;
+            floor = new int[floorSize][floorSize];
+            direction = "north";
+            pen = "up";
+            initialized = 1;
+            robotPosition = new int[]{0, 0};
+            return true;
+        }
+        else{
+            System.out.println("Size of array cannot be negative or zero");
+            return false;
+        }
+
     }
 
     //Method to Change Pen to Down
@@ -32,6 +45,10 @@ public class Robot {
     };
     //Method to Turn Right
     public int turnRight(){
+        if(initialized==0){
+            System.out.println("Please initialize the array first");
+            return 0;
+        }
         switch (direction) {
             case "north":
                 direction = "east";
@@ -49,7 +66,11 @@ public class Robot {
         return 0;
     };
     //Method to Turn Left
-    public int turnLeft(){
+    public String turnLeft(){
+        if(initialized==0){
+            System.out.println("Please initialize the array first");
+            return "";
+        }
         switch (direction) {
             case "north":
                 direction = "west";
@@ -64,26 +85,38 @@ public class Robot {
                 direction = "south";
                 break;
         }
-        return 0;
+        return direction;
     };
     //Method to display current position
     public String showCurrentPositionStatus(){
-        String position = "Position: " + Integer.toString(robotPosition[0]) + ", " + Integer.toString(robotPosition[1]);
+        if(initialized==0){
+            System.out.print("Please initialize the array first");
+            return "";
+        }
+
+        String position = "Position: " + robotPosition[0] + ", " + robotPosition[1];
         String penStatus = "Pen: " + pen;
         String face = "Facing: " + direction;
-
         return position + " - " + penStatus + " - " + face;
+
     }
-    public int moveForward(int spaces){
+    //Method to make the Robot move
+    public boolean moveForward(int spaces){
+        if(initialized==0){
+            System.out.println("Please initialize the array first");
+            return false;
+        }
         int pos1 = robotPosition[1];
         int pos0 = robotPosition[0];
+        //Check the direction in which robot is facing
+        //Then check if desired motion is not exceeding available space, then move, else display error message
         switch (direction) {
             case "north":
                 robotPosition[1] =  ((floorSize - robotPosition[1]) - spaces - 1)>=0 ? robotPosition[1] + spaces: robotPosition[1];
                 if(pen == "down"){
                     for (int i = pos1; i <= robotPosition[1]; i++){
                         floor[robotPosition[0]][i] = 1;
-                    };
+                    }
                 }
                 break;
             case "south":
@@ -91,7 +124,7 @@ public class Robot {
                 if(pen == "down"){
                     for (int i = pos1; i <=robotPosition[1]; i--){
                         floor[robotPosition[0]][i] = 1;
-                    };
+                    }
                 }
                 break;
             case "east":
@@ -111,26 +144,37 @@ public class Robot {
                 }
                 break;
         }
-        return 10;
+        return true;
     }
+    //Method to print Robot traversed path
     public void displayMatrix(){
+        if(initialized==0){
+            System.out.println("Please initialize the array first");
+            return;
+        }
+
         for (int i = floorSize-1; i>=0; i--){
-            System.out.print(String.format("%-1s%-1d","",i));
+            //Format the output, so we have equal spaces everywhere
+            //Print the indices for y-axis
+            System.out.print(String.format("%-3s%-3d","",i));
             for (int j = 0; j<floorSize; j++){
                 if(floor[j][i] == 1)
                 {
-                    System.out.print(String.format("%-1s%-1s","","*"));
+                    //If the array value is 1, print it as "*"
+                    System.out.print(String.format("%-3s%-3s","","*"));
                 }
                 else
                 {
-                    System.out.print(String.format("%-1s%-1s",""," "));
+                    //If the array value is 0, print it as " "
+                    System.out.print(String.format("%-3s%-3s",""," "));
                 }
             }
             System.out.println();
         }
-        System.out.print(String.format("%-1s",""));
+        //Print the indices for x-axis
+        System.out.print(String.format("%-6s",""));
         for(int i=0;i<floorSize;i++){
-            System.out.print(String.format("%-1s%-1d","",i));
+            System.out.print(String.format("%-3s%-3d","",i));
         }
         System.out.println();
     }
